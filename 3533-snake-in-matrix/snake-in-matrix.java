6d159254502b412006c300cdb3252034434cd47e
Somnath Dajiba Gatkul <somnathgatkul096@gmail.com>
2024-10-21 20:01:29 +0530

    Time: 2 ms (94.40%) | Memory: 44.4 MB (34.29%) - LeetSync

--- 3533-snake-in-matrix/snake-in-matrix.java
+++ 3533-snake-in-matrix/snake-in-matrix.java
@@ -1,15 +1,33 @@
-class Solution {
-    public int finalPositionOfSnake(int n, List<String> commands) {
-        HashMap<String,Integer> map = new HashMap<>();
-        map.put("DOWN",n);
-        map.put("UP",-n);
-        map.put("RIGHT",1);
-        map.put("LEFT",-1);
-        int ans = 0;
-        for(int i = 0;i<commands.size();i++){
-            ans = ans + map.get(commands.get(i));
-        }
+import java.util.*;
 
-        return ans;
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> c) {
+        int[][] v = new int[n][n];
+        
+        // Initialize the grid
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                v[i][j] = (i * n) + j;
+            }
+        }
+        
+        int x = 0, y = 0;
+        int k = c.size();
+        
+        // Process each command
+        for (int i = 0; i < k; i++) {
+            String direction = c.get(i);
+            if (direction.equals("UP")) {
+                x -= 1;
+            } else if (direction.equals("RIGHT")) {
+                y += 1;
+            } else if (direction.equals("DOWN")) {
+                x += 1;
+            } else if (direction.equals("LEFT")) {
+                y -= 1;
+            }
+        }
+        
+        return v[x][y];
     }
 }
