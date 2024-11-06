669c3bf81a08e33f826d257f19e5ad1191c50a5d
Somnath Dajiba Gatkul <somnathgatkul096@gmail.com>
2024-11-06 18:25:52 +0530

    Time: 17 ms (24.06%) | Memory: 47.4 MB (89.34%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            
+            if (!res.containsKey(key)) {
+                res.put(key, new ArrayList<>());
+            }
+            res.get(key).add(s);
+        }
+
+        return new ArrayList<>(res.values());
+    }
+}
