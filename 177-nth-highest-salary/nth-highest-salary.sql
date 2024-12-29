b709f43c0b0f9fd643ed17576e757fad310d56a2
Somnath Dajiba Gatkul <somnathgatkul096@gmail.com>
2024-12-29 18:11:35 +0530

    Time: 660 ms (26.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE  A INT;
+SET A = N-1;
+RETURN (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT A,1
+  );
+END
